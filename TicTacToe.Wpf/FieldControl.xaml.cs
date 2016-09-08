0c32499fb686fda09298828a26205e953c558a6a
m.gutekunst <m.gutekunst@GENERIC-NB22>
2016-09-08 11:28:53 +0200

    created a GameField usercontrol

---
+++ TicTacToe.Wpf/FieldControl.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using TicTactoe.Lib;
+
+namespace TicTacToe.Wpf
+{
+    /// <summary>
+    /// Interaction logic for FieldControl.xaml
+    /// </summary>
+    public partial class FieldControl : UserControl
+    {
+
+        public static readonly DependencyProperty GameFieldProperty = DependencyProperty.Register("GameField", typeof(GameField), typeof(FieldControl),
+            new PropertyMetadata(default(GameField)));
+
+        public GameField GameField
+        {
+            get { return (GameField) GetValue(GameFieldProperty); }
+            set { SetValue(GameFieldProperty, value); }
+        }
+
+        public FieldControl()
+        {
+            InitializeComponent();
+        }
+
+        private void FieldControl_OnMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            GameField.SetState(FieldState.X);
+        }
+    }
+}
