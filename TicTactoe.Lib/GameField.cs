5b45d5c66334c3c971e3ee1012ab2f111dd6e9e5
Manuel Gutekunst <Manuel.Gutekunst@generic.de>
2016-09-08 11:10:08 +0200

    initial commit. Created GameField

---
+++ TicTactoe.Lib/GameField.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TicTactoe.Lib
+{
+    public enum FieldState
+    {
+        Unchecked,
+        X,
+        O
+    }
+    public class GameField : NotifyBaseClass
+    {
+        private FieldState _state;
+        public FieldState State
+        {
+            get { return _state; }
+            private set { _state = value; RaisePropertyChanged(); }
+        }
+
+        public void SetState(FieldState fieldState)
+        {
+            if (State != FieldState.Unchecked)
+                return;
+
+            State = fieldState;
+        }
+    }
+}
